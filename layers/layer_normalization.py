1ce9b250de1ba88e8bef7144de849716bf93da5b
rickzhang716 <rickzhang7@gmail.com>
2023-10-14 02:05:34 -0700

    explaining layer normalization in comments

--- layers/layer_normalization.py
+++ layers/layer_normalization.py
@@ -8,14 +8,28 @@ from torch import Tensor
 class LayerNormalization(nn.Module):
     def __init__(self, embedding_dimension: int, epsilon: float = 1e-12):
         super().__init__()
+
         self.embedding_dimension = embedding_dimension
         self.gamma = torch.zeros(embedding_dimension)
         self.beta = torch.ones(embedding_dimension)
         self.epsilon = epsilon
 
     def forward(self, input_tensor: Tensor):
-        mean = input_tensor.mean(dim=1, keepdim=True)
-        variance = input_tensor.var(dim=1, keepdim=True, correction=0)
+        # input_tensor[batch_size, length, embedding_dimension]
+        # output[batch_size, length, embedding_dimension]
+        '''
+        by taking mean/variance, we reduce the dimensionality by 1, so these tensors are [batch_size,1,1].
+        Thus, for each batch, we have a mean/variance of all x_i's.
+        Next, we subtract input_tensor by mean, so for each batch, we subtract each x_i
+        in that batch by the mean for that batch, and divide by the root of variance for that batch.
+        Thus, we are left with a tensor [batch_size,length,embedding_dimension].
+        The gamma and beta tensors are broadcastable, meaning that, although they are of size[embedding_dimension],
+        when we element-wise add/multiply to our tensor y [batch_size,length, embedding_dimension],
+        we will be able to automatically expand gamma and beta to the correct size.
+        This is also why we can keep epsilon as a single value.
+        '''
+        mean = input_tensor.mean(dim=(1, 2), keepdim=True)
+        variance = input_tensor.var(dim=(1, 2), keepdim=True, correction=0)
         y = (input_tensor - mean) / torch.sqrt(variance + self.epsilon)
         output = self.gamma * y + self.beta
         return output
